899f4089fcade3648f2828e08bdc98edca1258f8
Tang <942975406@qq.com>
2019-01-23 15:31:57 +0800

    引导页第一页

---
+++ wdmovie/src/main/java/com/bw/movie/guide/fragment/GuideFirstFragment.java
@@ -0,0 +1,22 @@
+package com.bw.movie.guide.fragment;
+
+import android.view.View;
+
+import com.bw.movie.fragmnet.BaseFragment;
+
+public class GuideFirstFragment extends BaseFragment {
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView(View view) {
+
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return 0;
+    }
+}
