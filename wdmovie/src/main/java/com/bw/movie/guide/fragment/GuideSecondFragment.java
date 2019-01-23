1e3844d7cfe56fac551287f46e1bc889c6f29f36
guoxinyu <guoxinyu@qq.com>
2019-01-23 15:41:33 +0800

    第二个引导页

---
+++ wdmovie/src/main/java/com/bw/movie/guide/fragment/GuideSecondFragment.java
@@ -0,0 +1,29 @@
+package com.bw.movie.guide.fragment;
+
+import android.view.View;
+
+import com.bw.movie.fragmnet.BaseFragment;
+
+public class GuideSecondFragment extends BaseFragment {
+    /**
+     * 初始化数据
+     * */
+    @Override
+    protected void initData() {
+
+    }
+    /**
+     * 初始化view
+     * */
+    @Override
+    protected void initView(View view) {
+
+    }
+    /**
+     * 加载布局
+     * */
+    @Override
+    protected int getLayoutResId() {
+        return 0;
+    }
+}
