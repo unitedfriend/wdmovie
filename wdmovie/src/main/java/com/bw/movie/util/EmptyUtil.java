b843423951b581dd87917ad1f4746591ea643b00
guoxinyu <guoxinyu@qq.com>
2019-01-24 18:33:41 +0800

    维度影院登录1

---
+++ wdmovie/src/main/java/com/bw/movie/util/EmptyUtil.java
@@ -0,0 +1,16 @@
+package com.bw.movie.util;
+
+import android.text.TextUtils;
+
+/**
+  * @作者 GXY
+  * @创建日期 2019/1/24 14:42
+  * @描述 非空判断工具类
+  *
+  */
+public class EmptyUtil {
+    //非空判断
+    public static boolean isNull(String name,String password){
+        return !TextUtils.isEmpty(name)&&!TextUtils.isEmpty(password);
+    }
+}
